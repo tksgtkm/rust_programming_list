5b9fbfea9e7fac5a8f98ac73fae2d8f89f2667c2
tksgtkm <h9cd342fd7jgx36@gmail.com>
2023-11-09 01:59:07 +0900

    first commit

---
+++ function_pointer/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    do_it(add, 10, 2);
+    do_it(mul, 10, 2);
+}
+
+fn do_it(f: fn(u32, u32) -> u32, a : u32, b : u32) {
+    println!("{}", f(a, b));
+}
+
+fn add(a : u32, b : u32) -> u32 {
+    a + b;
+}
+
+fn mul(a : u32, b : u32) -> u32 {
+    a * b;
+}
